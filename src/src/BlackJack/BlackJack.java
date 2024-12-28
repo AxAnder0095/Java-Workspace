4c4437b03af5c1dd031513980011af45629e2c55
Frenguin <104327737+Frenguin@users.noreply.github.com>
2024-12-28 15:32:43 -0600

    creating blackjack

---
+++ src/src/BlackJack/BlackJack.java
@@ -0,0 +1,13 @@
+package BlackJack;
+
+import java.util.ArrayList;
+
+public class BlackJack {
+    public static void main(String[] args) {
+        ArrayList<String> deck = createDeck();
+    }
+
+    private static ArrayList<String> createDeck() {
+        return null;
+    }
+}
